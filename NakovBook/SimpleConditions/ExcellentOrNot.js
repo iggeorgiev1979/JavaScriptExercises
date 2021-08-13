173e11f11afc4f694492628db3ee17e0228e6f04
iggeorgiev1979 <iggeorgiev1979@gmail.com>
2021-08-13 17:45:48 +0300

    Added file

---
+++ NakovBook/SimpleConditions/ExcellentOrNot.js
@@ -0,0 +1,8 @@
+function grade(arg){
+    let my_grade = parseFloat(arg);
+    if (my_grade >= 5.5){
+        console.log("Excellent!");
+    }else{
+        console.log("Not excellent.");
+    }
+}
