6ddf66f8f7057f74cf4bd6f0f2dfa1ac5cb54474
iggeorgiev1979 <iggeorgiev1979@gmail.com>
2021-08-13 23:21:21 +0300

    Added file

---
+++ NakovBook/SimpleConditions/EqualWords.js
@@ -0,0 +1,9 @@
+function solve([arg1, arg2]){
+    arg1 = arg1.toLowerCase();
+    arg2 = arg2.toLowerCase();
+    if (arg1 == arg2){
+        console.log("yes")
+    }else{
+        console.log("no")
+    }
+}
