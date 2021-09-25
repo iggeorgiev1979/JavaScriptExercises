fdd74eaa75d77fc2f0d9b93f1768e0af067200e4
iggeorgiev1979 <iggeorgiev1979@gmail.com>
2021-09-25 23:49:10 +0300

    Added file

---
+++ Advanced/DOM/ShowMore.js
@@ -0,0 +1,6 @@
+function showText() {
+    let element = document.getElementById('text');
+    let secondElement = document.getElementById('more');
+    secondElement.style.display = 'none';
+    element.style.display = 'inline';
+}
